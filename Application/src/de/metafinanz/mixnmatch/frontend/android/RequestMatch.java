5119a3da2d8c9638cb22551243a4d79df3fcd815
Tobias Schnupp <tobias.schnupp@metafinanz.de>
2011-04-21 19:26:34 +0200

    Fehlerbehebung

--- Application/src/de/metafinanz/mixnmatch/frontend/android/RequestMatch.java
+++ Application/src/de/metafinanz/mixnmatch/frontend/android/RequestMatch.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
