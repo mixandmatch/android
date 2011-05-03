5e19bee925d9ff3abff654f39a668522f5c1ad2c
unknown <MR@.metafinanz.office>
2011-05-03 20:10:54 +0200

    location dialog

---
+++ Application/src/de/metafinanz/mixnmatch/frontend/android/LocationDialog.java
@@ -0,0 +1,21 @@
+package de.metafinanz.mixnmatch.frontend.android;
+
+import android.os.Bundle;
+
+public class LocationDialog extends MapActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.location_dialog);
+
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+}
