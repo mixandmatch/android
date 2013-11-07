bf4df59cd12c54a01a3a6c90542f612c569195a9
Tobias Siebert <tobias.siebert@metafinanz.de>
2013-11-07 15:07:25 +0100

    Make SharedPreferences Key static

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Mix & Match Android
 Build
 -----
 
-To build this project an Android SDK with build level 16 is needed. 
+To build this project an Android SDK with build level 16 is needed.  
 The android maven plugin needs the ANDROID_HOME environment variable set to the android sdk folder.  
 For example:  
 D:\path\to\Android\android-sdk
--- src/main/java/de/metafinanz/mixmatch/activities/MixMatchActivity.java
+++ src/main/java/de/metafinanz/mixmatch/activities/MixMatchActivity.java
@@ -5,32 +5,32 @@ import android.content.SharedPreferences;
 
 public abstract class MixMatchActivity extends Activity {
 
-	private final String MIXMATCH_PREFS = "mixmatch_preferences";
-	private final String KEY_USERNAME = "mixmatch_username";
-	
+	private static final String MIXMATCH_PREFS = "mixmatch_preferences";
+	private static final String KEY_USERNAME = "mixmatch_username";
+
 	private String username;
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		SharedPreferences settings = getSharedPreferences(MIXMATCH_PREFS, MODE_PRIVATE);
+		SharedPreferences settings = getSharedPreferences(MIXMATCH_PREFS,
+				MODE_PRIVATE);
 		username = settings.getString(KEY_USERNAME, "");
 	}
-
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		SharedPreferences settings = getSharedPreferences(MIXMATCH_PREFS, MODE_PRIVATE);
+		SharedPreferences settings = getSharedPreferences(MIXMATCH_PREFS,
+				MODE_PRIVATE);
 		settings.edit().putString(KEY_USERNAME, username);
 		settings.edit().commit();
 	}
 
-
 	protected String getUsername() {
 		return username;
 	}
-	
+
 	protected void setUsername(String username) {
 		this.username = username;
 	}
