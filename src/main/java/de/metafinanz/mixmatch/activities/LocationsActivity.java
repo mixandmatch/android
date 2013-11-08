cec2707786eb18bca2e4e0c8f594a1bbb172e50a
ulfjaeger <ujaeger@gmx.de>
2013-11-08 01:38:12 +0100

    changed listitem color

--- src/main/java/de/metafinanz/mixmatch/activities/LocationsActivity.java
+++ src/main/java/de/metafinanz/mixmatch/activities/LocationsActivity.java
@@ -2,13 +2,17 @@ package de.metafinanz.mixmatch.activities;
 
 import java.util.List;
 
+import android.R.anim;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import de.metafinanz.mixmatch.R;
 import de.metafinanz.mixmatch.domain.Location;
 import de.metafinanz.mixmatch.service.MixMatchService;
@@ -22,7 +26,14 @@ public class LocationsActivity extends MixMatchActivity {
 		// Show the Up button in the action bar.
 		setupActionBar();
 		List<Location> list = MixMatchService.getInstance().getLocations();
-		ArrayAdapter<Location> adapter = new ArrayAdapter<Location>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+		ArrayAdapter<Location> adapter = new ArrayAdapter<Location>(getApplicationContext(), android.R.layout.simple_list_item_1, list) {
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				TextView textView = (TextView) super.getView(position, convertView, parent);
+				textView.setTextColor(getResources().getColor(android.R.color.black));
+				return textView;
+			}
+		};
 		
 		ListView view = (ListView) findViewById(R.id.locationsListView);
 		view.setAdapter(adapter);
