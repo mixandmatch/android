bed873b6f0a02fe402fcdbffb6b527023cd1ca0c
ulfjaeger <ujaeger@gmx.de>
2014-03-14 16:40:52 +0100

    disable Home-Button on first screen

--- src/main/java/de/metafinanz/mixmatch/activities/MainActivity.java
+++ src/main/java/de/metafinanz/mixmatch/activities/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends MixMatchActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intentAppointments = new Intent(this, AppointmentsActivity.class);
+        getActionBar().setHomeButtonEnabled(false);
+        getActionBar().setDisplayHomeAsUpEnabled(false);
     }
     
     @Override
